04f9c4a3ad0c8c07f86d79cfe54455eb3d981e13
G0C001 <gokulvbe1303@gmail.com>
2024-12-11 16:53:03 +0530

    bluewhale

--- BlueWale/DB_creation.py
+++ BlueWale/DB_creation.py
@@ -6,10 +6,10 @@ from django.http import JsonResponse
 from django.views.decorators.http import require_GET, require_http_methods
 from django.views.decorators.csrf import csrf_exempt
 
-TOKEN="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"
-OWNER="G0C001"
-REPO="database"
-BRANCH= "main"
+
+load_dotenv()
+TOKEN, OWNER, REPO, BRANCH = map(os.getenv, ["GITHUB_TOKEN", "GITHUB_OWNER", "GITHUB_REPO", "GITHUB_BRANCH"])
+
 
 @csrf_exempt
 @require_GET
