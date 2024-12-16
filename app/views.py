70ff41cb808a6928a29a633f83ebef7a8f15519c
G0C001 <gokulvbe1303@gmail.com>
2024-12-16 20:39:39 +0530

    new

--- app/views.py
+++ app/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render, redirect
 import sqlite3
+import requests
 from datetime import datetime
 current_datetime = datetime.now()
 formatted_datetime = current_datetime.strftime('%Y-%m-%d %H:%M:%S')
 GLOBAL_USERNAME = None
+
+def db(query):
+    url = "http://127.0.0.1:8000/API/database"
+    params = {
+        "token": "QldPL0JXT19NQUlOX1VTRVJTLmRi",
+        "query": query
+    }
+    response = requests.get(url, params=params)
+    if response.status_code != 200:
+        return []
+    else:
+       return response.json()['results']
+
+
 def register(request):
     global GLOBAL_USERNAME
     if request.method == 'POST':
@@ -12,11 +27,9 @@ def register(request):
         if action == 'login':
             username = request.POST.get('username')
             password = request.POST.get('password')
-            connection = sqlite3.connect('BLUEWHALE_USER_REPORT.db')
-            cursor = connection.cursor()
-            query = "SELECT * FROM user_data WHERE username = ? AND password = ?"
-            cursor.execute(query, (username, password))
-            result = cursor.fetchall()
+
+            query = f"SELECT * FROM BWO_WEBSITE_USERS WHERE username = '{username}' AND password = '{password}'"
+            result = db(query)
 
             if result != []:
                 GLOBAL_USERNAME = result[0][1]
@@ -27,16 +40,13 @@ def register(request):
             username = request.POST.get('username')
             password = request.POST.get('password')
             email = request.POST.get('email')
-            print(username, password, email)
-            connection = sqlite3.connect('BLUEWHALE_USER_REPORT.db')
-            cursor = connection.cursor()
-            query = """INSERT INTO user_data (username, password, email, created_at) VALUES (?, ?, ?, ?)"""
-            cursor.execute(query, (username, password, email, formatted_datetime))
-            connection.commit()
 
+            query = f"""INSERT INTO BWO_WEBSITE_USERS (username, password, email, created_at) 
+                        VALUES ('{username}', '{password}', '{email}', '{formatted_datetime}')"""
+            print(query)
+            db(query)
 
     return render(request, 'register.html')
-
 
 
 def home(request):
